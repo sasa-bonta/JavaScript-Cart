613afb125a88ebade5b8fa3fcc9d503ad05a0286
sasa-bonta <bonta2000@mail.ru>
2021-07-15 14:10:17 +0300

    cart on collapse

--- resources/js/products.js
+++ resources/js/products.js
@@ -6,56 +6,56 @@ let pizzasArray = [
         name: "Affumicata",
         price: 95,
         description: "description",
-        image: "9c36508a696673f98fea8634a908d3ad.jpg",
+        image: "images/pizzas/9c36508a696673f98fea8634a908d3ad.jpg",
     },
     {
         id: 2,
         name: "Cheese&Spinach",
         price: 95,
         description: "description",
-        image: "15cda0097dd4bd4227af9247a663f935.jpg",
+        image: "images/pizzas/15cda0097dd4bd4227af9247a663f935.jpg",
     },
     {
         id: 3,
         name: "5 Cheese",
         price: 90,
         description: "description",
-        image: "d427104f35bfee2b727079d5e414df43.jpg",
+        image: "images/pizzas/d427104f35bfee2b727079d5e414df43.jpg",
     },
     {
         id: 4,
         name: "Barbeque",
         price: 90,
         description: "description",
-        image: "38c60145ae43db28fe54f871e88d72ca.jpg",
+        image: "images/pizzas/38c60145ae43db28fe54f871e88d72ca.jpg",
     },
     {
         id: 5,
         name: "Rancho",
         price: 88,
         description: "description",
-        image: "266518ecf029ceeec3b01c094c8ca7f0.jpg",
+        image: "images/pizzas/266518ecf029ceeec3b01c094c8ca7f0.jpg",
     },
     {
         id: 6,
         name: "Prosciutto",
         price: 115,
         description: "description",
-        image: "e3d52e592e43fe7f522fe2ccb527db6f.jpg",
+        image: "images/pizzas/e3d52e592e43fe7f522fe2ccb527db6f.jpg",
     },
     {
         id: 7,
         name: "Pepperoni",
         price: 90,
         description: "description",
-        image: "6fe9ec1ff95274fb46723971624ede0b.jpg",
+        image: "images/pizzas/6fe9ec1ff95274fb46723971624ede0b.jpg",
     },
     {
         id: 8,
         name: "Margherita",
         price: 75,
         description: "description",
-        image: "38a8de519a66ddb36a5f650b87218890.jpg",
+        image: "images/pizzas/38a8de519a66ddb36a5f650b87218890.jpg",
     },
 ];
 
@@ -83,7 +83,7 @@ pizzasArray.forEach((pizza, index) => {
     products += !isEmpty(pizza) ?
         `<div class="col col-6 col-lg-3">
             <div class="card text-white h-100">
-                <img src="images/pizzas/${pizza.image}" class="product-image" alt="${pizza.image}">
+                <img src="${pizza.image}" class="product-image" alt="${pizza.image}">
                 <h4>${pizza.name}</h4>
                 <div class="btn-holder">
                     <button onclick="addToCart(${pizza.id});" class="w-100">
