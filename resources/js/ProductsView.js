1daf658045774f68521a688b22281a46d31e3a01
sasa-bonta <bonta2000@mail.ru>
2021-07-24 18:23:10 +0300

    test

--- resources/js/ProductsView.js
+++ resources/js/ProductsView.js
@@ -3,6 +3,15 @@ import "./Product"
 class ProductsView {
     constructor({ onBtnClick }) {
         this.div = document.getElementById("pizzas");
+        this.action = onBtnClick;
+    }
 
+    displayProducts(products) {
+        const row = document.createElement("div");
+        row.className = "row";
+
+        products.forEach((pizza) => {
+
+        });
     }
 }
