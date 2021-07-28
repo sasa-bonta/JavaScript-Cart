ee53039226828dcbd6188685457086b58a075133
sasa-bonta <bonta2000@mail.ru>
2021-07-28 17:38:38 +0300

    justify-content-around

--- resources/js/cart/CartItem.js
+++ resources/js/cart/CartItem.js
@@ -1,7 +1,5 @@
 export class CartItem {
 
-    s3 = "&nbsp;&nbsp;&nbsp;";
-
     constructor({id, name, price, description, image, quantity}) {
         this.id = id;
         this.name = name;
@@ -15,14 +13,14 @@ export class CartItem {
         return `
         <img src="${this.image}" class="cart-image" alt="item.name"> 
                                <div class="product-cart">
-                                   ${this.name}: ${this.s3}
-                                   <!--<button class="btn btn-secondary addOne"> - </button>-->
-                                   ${this.quantity} pcs. 
-                                   <!--<button class="btn btn-secondary removeOne" > + </button>-->
-                                   ${this.s3 + this.s3} ${priceAll} MDL 
-                                   <button class="btn btn-danger delete-btn">
-                                       <i class="bi-trash"></i>
-                                   </button>
+                                   <div class="d-flex justify-content-around">
+                                       <div>${this.name}:</div>
+                                       <div>${this.quantity} pcs. </div>
+                                       <div>${priceAll} MDL </div>
+                                       <button class="btn btn-danger delete-btn">
+                                           <i class="bi-trash"></i>
+                                       </button>
+                                   </div>                            
                                </div><br>
         `;
     }
