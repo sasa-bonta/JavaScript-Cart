3dc2a5d5dd44bf8924bd97575cab77e525474b2a
sasa-bonta <bonta2000@mail.ru>
2021-07-29 22:34:39 +0300

    muscula works!!!

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -10,9 +10,11 @@ const error1 = document.getElementById("error1");
 const error2 = document.getElementById("error2");
 
 error1.addEventListener('click', () => {
-
+    localStorage.test = '["Hello", 3.14, true, "name": "Joe"]';
+    let ninune = storage.load("test");
+    console.log(ninune);
 });
 
 error2.addEventListener('click', () => {
-
+    productsView.showRows();
 });
